b9821949ae7744d0d2fbaf87f3036ec703892f13
biohazard333 <130109964+biohazard333@users.noreply.github.com>
2023-10-13 14:55:49 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+document.getElementById('run-script').addEventListener('click', function() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            console.log(this.responseText);
+        }
+    };
+    xhttp.open("GET", "app.py", true);
+    xhttp.send();
+});
