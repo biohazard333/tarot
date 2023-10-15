434268a30a55f42260cf3599bb1bbaee1dc3c649
biohazard333 <130109964+biohazard333@users.noreply.github.com>
2023-10-15 14:58:42 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,36 +1,41 @@
-from flask import Flask, render_template, request
 import os
 import openai
 import random
 from gtts import gTTS 
 import subprocess
 import requests
+import streamlit as st
 
-app = Flask(__name__)
+# Configuración de OpenAI
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
-openai.api_key = "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"
+# Lista de cartas de tarot
+cartas_tarot = ["amantes", "carroza", "colgado", "diablo", "emperador",
+                "emperatriz", "ermitaño", "estrella", "fortuna",
+                "fuerza", "juicio", "justicia", "loco", "luna",
+                "mago", "muerte", "mundo", "sacerdote", "sacerdotiza",
+                "sol", "templanza", "torre"]
 
-cartas_tarot = [
-    "amantes", "carroza", "colgado", "diablo", "emperador",
-    "emperatriz", "ermitaño", "estrella", "fortuna",
-    "fuerza", "juicio", "justicia", "loco", "luna",
-    "mago", "muerte", "mundo", "sacerdote", "sacerdotiza",
-    "sol", "templanza", "torre"
-]
+# Ruta para las imágenes
+current_dir = os.getcwd()
+static_images_path = os.path.join(current_dir, "static/images")
 
+# Función para seleccionar cartas de tarot al azar
 def seleccionar_cartas_tarot(cantidad):
     return random.sample(cartas_tarot, cantidad)
 
+# Función para generar audio a partir de texto
 def generate_audio(text, output_path):
     tts = gTTS(text, lang='es')
     tts.save(output_path)
 
+# Función para generar video con Wav2Lip
 def generate_video(audio_path, face_image_path, video_path):
     command = f"python3 inference.py --checkpoint_path checkpoints/wav2lip_gan.pth --face face.jpg --audio {audio_path} --outfile {video_path} --nosmooth"
     process = subprocess.run(command, shell=True, text=True)
     return process.returncode
 
-# Descargar el archivo wav2lip_gan.pth si no existe
+# Comprobación y descarga del archivo de checkpoint si no existe
 checkpoint_path = "checkpoints/wav2lip_gan.pth"
 if not os.path.exists(checkpoint_path):
     url = "https://huggingface.co/spaces/salomonsky/tarot/resolve/main/checkpoints/wav2lip_gan.pth"
@@ -40,33 +45,29 @@ if not os.path.exists(checkpoint_path):
         os.makedirs(os.path.dirname(checkpoint_path), exist_ok=True)
         with open(checkpoint_path, "wb") as file:
             file.write(response.content)
-        print("Archivo descargado exitosamente.")
-    else:
-        print("Error al descargar el archivo.")
 
-# Crear la carpeta 'temp' si no existe
-os.makedirs('temp', exist_ok=True)
+# Configuración de la página Streamlit
+st.title("Tirada de Tarot")
 
-from flask import Flask, render_template, request
-import os
-import openai
-import random
-from gtts import gTTS 
-import subprocess
-import requests
+# Sección de pregunta
+name = st.text_input("1. Haz tu pregunta:")
 
-app = Flask(__name__)
+# Sección de salida de cartas
+if name:
+    cartas = seleccionar_cartas_tarot(4)
+    st.write(f"2. Salida de 4 cartas aleatorias: {', '.join(cartas)}")
 
-# Resto de tu código...
+    # Mostrar imágenes de las cartas
+    st.subheader("Imágenes de las cartas:")
+    col1, col2, col3, col4 = st.columns(4)  # Definir 4 columnas
 
-def generate_output(name):
-    if not name:
-        return None, "El campo de nombre es obligatorio."
+    for i, carta in enumerate(cartas):
+        image_path = os.path.join(static_images_path, f"{carta}.png")
+        col = [col1, col2, col3, col4][i]  # Seleccionar la columna correspondiente
+        col.image(image_path, caption=carta, use_column_width=True, width=100)
 
-    cartas_seleccionadas = seleccionar_cartas_tarot(4)
-
-    prompt = f"Interpretando las cartas sobre la pregunta sobre tu lectura es: {', '.join(cartas_seleccionadas)}."
-
+    # Sección de interpretación de GPT-3
+    prompt = f"Interpretando las cartas sobre la pregunta sobre tu lectura es: {', '.join(cartas)}."
     response = openai.Completion.create(
         engine="text-davinci-003",
         prompt=prompt,
@@ -75,34 +76,20 @@ def generate_output(name):
         n=1,
         stop=None,
     )
-    gpt3_output = response.choices[0].text.strip()
+    
+    if len(response.choices) > 0 and 'text' in response.choices[0]:
+        gpt3_output = response.choices[0].text.strip()
+        st.write(f"3. Interpretación del GPT-3: {gpt3_output}")
 
-    if len(response.choices) == 0 or 'text' not in response.choices[0]:
-        return None, "No se pudo generar el texto."
+        # Sección de video generado por Wav2Lip
+        audio_path = "temp/temp.wav"
+        video_dir = "videos/"
+        os.makedirs(video_dir, exist_ok=True)
+        video_path = f"{video_dir}video.mp4"
+        face_image_path = "face.jpg"
 
-    return cartas_seleccionadas, gpt3_output
+        generate_audio(gpt3_output, audio_path)
+        return_code = generate_video(audio_path, face_image_path, video_path)
 
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    if request.method == 'POST':
-        name = request.form['pregunta']
-        cartas, gpt_output = generate_output(name)
-        if cartas and gpt_output:
-            audio_path = "temp/temp.wav"
-            video_dir = "videos/"
-            os.makedirs(video_dir, exist_ok=True)
-            video_path = f"{video_dir}video.mp4"
-            face_image_path = "face.jpg"
-
-            generate_audio(gpt_output, audio_path)
-            return_code = generate_video(audio_path, face_image_path, video_path)
-            if return_code == 0 and os.path.isfile(video_path):
-                return redirect('https://biohazard333.github.io/tarot', cartas=cartas, gpt_output=gpt_output, video_path=video_path)
-            else:
-                return "No se pudo generar el video."
-        else:
-            return "Error al generar las cartas y la interpretación."
-    return redirect('https://biohazard333.github.io/tarot')
-
-if __name__ == '__main__':
-    app.run(debug=True)
+        if return_code == 0 and os.path.isfile(video_path):
+            st.video(video_path, format="video/mp4")
